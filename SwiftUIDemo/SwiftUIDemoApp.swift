a8eb007b10d3f17ac70a9d0e93e757f73d904af5
yorn <yuanliang@suunto.cn>
2024-12-11 19:37:37 +0800

    first commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by 袁量 on 2024/12/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
